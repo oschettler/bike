7b067809e10e8a57840046fc56917aa30f272403
Olav Schettler <olav@schettler.net>
2023-10-24 23:17:56 +0200

    Workimng site

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
-<!doctype html>
+<?php
+include('env.php');
+
+session_start();
+$message = isset($_SESSION['message']) ? $_SESSION['message'] : null;
+unset($_SESSION['message']);
+$text = isset($_SESSION['text']) ? $_SESSION['text'] : '';
+unset($_SESSION['text']);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  if (empty($_POST['zahl']) || $_POST['zahl'] != 52) {
+    $_SESSION['message'] = 'Wie ist die Rahmengröße?';
+    $_SESSION['text'] = $_POST['text'];
+  }
+  else {
+    $_SESSION['message'] = 'Danke für die Hilfe';
+    $_SESSION['text'] = '';
+    mail($mail, 'BIKE', $_POST['text']);
+  }
+  header('Location: /');
+}
+
+?><!doctype html>
 <html class="no-js" lang="de">
   <head>
     <meta charset="utf-8">
@@ -12,9 +34,85 @@
     <link rel="manifest" href="/site.webmanifest">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
+    <style>
+    body {
+      background-image: url('bg.jpg');
+      background-repeat: no-repeat;
+      background-attachment: fixed;
+      background-size: cover;
+    }
+    section {
+	margin: 100px auto auto 40px;
+	padding: 40px;
+	width: 50%;
+	background: rgba(255,255,255,0.8);
+	font-family: arial, helvetica, sans-serif;
+	line-height: 1.4em;
+    }
+    textarea, button {
+	display: block;
+    }
+    textarea, input,  button {
+	padding: 4px;
+	border: solid 1px gray;
+	border-radius: 4px;
+	margin-bottom: 10px;
+    }
+    fieldset {
+	border: none;
+	padding: 0;
+	
+    }
+    textarea, fieldset {
+	width: 100%;
+    }
+    input {
+	width: 4em;
+    }
+    #message {
+	width: 80%;
+	border: solid 1px green;
+	background: #DFD;
+        padding: 10px;
+    }
+    </style>
   </head>
   <body>
-    <p>Fahrrad geklaut</p>
+    <section>
+      <h1>Fahrrad geklaut</h1>
+      <p>Am Montag, 24.10. um 14 Uhr klaute dieser Mann das Fahrrad unserer Tochter aus unserem Vorgarten in der Bonner Altstadt. Wir hätten es gerne zurück.</p>
+      <div>
+	Marke: <strong>Winora</strong><br> 
+        Typ: <strong>Domingo 24	Diamant</strong><br>
+	Farbe: <strong>oliv/orange</strong></br>
+	Rahmengröße: <strong>52cm</strong><br>
+        Rahmennummer: <strong>LY02032210</strong><br>
+        Codierung: <strong>BN 00 01616 014 LS 22</strong>
+      </div>
+
+      <p>Hinweis gerne an <strong>0160-1529500</strong> (SMS, Signal)</p>
+      <?php
+      if ($message):
+      ?>
+      <div id="message">
+        <?= $message ?>
+      </div>
+      <?php
+      else:
+      ?>
+      <form method="POST">
+	<h2>Oder direkt hier:</h2>
+	<textarea name="text" rows="4" placeholder="Hast du das Fahrrad oder den Mann gesehen? Schreibe mir hier gerne einen Hinweis."><?= $text ?></textarea>
+	<fieldset>
+	  <label for="zahl">Spamschutz (Rahmengröße als Zahl):</label>
+	  <input name="zahl" type="number">
+	</fieldset>
+	<button type="submit">Abschicken</button>
+      </form>
+      <?php
+      endif;
+      ?>
+    </section>
   </body>
 </html>
 
